b7cbf25a4a79aa3db62dc992b5d1cf977f5ef05d
eakdemir1997 <96940740+eakdemir1997@users.noreply.github.com>
2022-11-06 20:01:51 +0300

    Loops

---
+++ src/javaIntro/Loops.java
@@ -0,0 +1,48 @@
+package javaIntro;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=10; i++) {
+			System.out.println(i);
+		}
+		
+		int sayac=0;
+		String ad="Erdinç";
+		
+		while(sayac<5) {
+			System.out.println(ad+"-"+sayac);
+			sayac++;			
+		}
+		
+		int x=15;
+		
+		do {
+			x+=5;
+			System.out.println(x);
+		}while(x !=35);
+		
+		
+		System.out.println("- Ýç içe for döngüsü -");
+		
+		
+		for(int i =0; i<5; i++) {
+			for(int j=5; j>i; j-- ) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("--------------------------------------");
+		
+		for(int i=1; i<=5; i++) {
+			for(int j=0; j<i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
